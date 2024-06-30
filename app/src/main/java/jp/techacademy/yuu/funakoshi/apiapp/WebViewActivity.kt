b4a8d415ac95b8b45d4db972e9cad83083a0e2c2
thebossthe <yamashiro1582@gmai.com>
2024-07-01 01:06:31 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuu/funakoshi/apiapp/WebViewActivity.kt
@@ -0,0 +1,82 @@
+package jp.techacademy.yuu.funakoshi.apiapp
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebViewClient
+import jp.techacademy.yuu.funakoshi.apiapp.databinding.ActivityWebViewBinding
+
+class WebViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWebViewBinding
+
+    private var isFavorite = false
+    private lateinit var favoriteShop: FavoriteShop
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWebViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // IntentからURLとIDを取得
+        val url = intent.getStringExtra(KEY_URL).toString()
+        val id = intent.getStringExtra(KEY_ID).toString()
+
+        Log.d("TEST","id" + id)
+
+        binding.webView.apply {
+            loadUrl(intent.getStringExtra(KEY_URL).toString())
+            webViewClient = WebViewClient()
+        }
+
+        binding.WebfavoriteImageView.setOnClickListener {
+            if (isFavorite) {
+                deleteFavorite()
+            } else {
+                addFavorite()
+            }
+        }
+    }
+
+    private fun updateFavoriteImage() {
+        if (isFavorite) {
+            binding.WebfavoriteImageView.setImageResource(R.drawable.ic_star_border)
+        } else {
+            binding.WebfavoriteImageView.setImageResource(R.drawable.ic_star)
+        }
+    }
+
+    private fun addFavorite() {
+        FavoriteShop.insert(FavoriteShop().apply {
+            id = favoriteShop.id
+            name = favoriteShop.name
+            imageUrl = favoriteShop.imageUrl
+            url = favoriteShop.url
+        })
+        isFavorite = true
+        updateFavoriteImage()
+        Log.d("TEST","Webページでお気に入り追加")
+    }
+
+    private fun deleteFavorite() {
+        FavoriteShop.delete(favoriteShop.id)
+        isFavorite = false
+        updateFavoriteImage()
+        Log.d("TEST","Webページでお気に入り削除")
+    }
+
+    companion object {
+        private const val KEY_URL = "key_url"
+        private const val KEY_ID = "key_id"
+
+        fun start(activity: Activity, url: String,id: String) {
+            activity.startActivity(
+                Intent(activity, WebViewActivity::class.java).apply {
+                    putExtra(KEY_URL, url)
+                    putExtra(KEY_ID, id)
+                }
+            )
+        }
+    }
+}
